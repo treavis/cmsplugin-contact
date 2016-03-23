0041e24a200df8cbe3f10805fa885c9c8be8b34d
Matas Dailyda <matas@dailyda.com>
2016-03-23 19:40:53 +0200

    fix js

--- cmsplugin_contact/static/js/cmsplugin_contact.js
+++ cmsplugin_contact/static/js/cmsplugin_contact.js
@@ -1,38 +1,38 @@
-(function($){
-    "use strict";
-    $(function(){
+$(document).ready(function(){
     
-        $('#cmsplugin-contact-form').submit(function(evt){
-            var $form = $(this);
-            evt.preventDefault();
+    $('#cmsplugin-contact-form').submit(function(evt){
+        var $form = $(this);
+        evt.preventDefault();
 
-            function handleResponse(data){
-                if (data.ok) { // Success!
-                    $('#confirmation-message').fadeIn();
-                    $form.hide();
-                } else {
-                    if (typeof Recaptcha != "undefined") {
-                        Recaptcha.reload();
-                    }
-                    $('span.error_msg').remove();
-                    $.each(data, function(key, value){
-                       $('div.input-group.' + key).append('<span class="error_msg">' + value + '</span>');
-                    });
+        function handleResponse(data){
+            if (data.ok) { // Success!
+                $('#confirmation-message').fadeIn();
+                $form.hide();
+            } else {
+                if (typeof Recaptcha != "undefined") {
+                    Recaptcha.reload();
                 }
+                $('span.error_msg').remove();
+                $.each(data, function(key, value){
+                   $('div.input-group.' + key).append('<span class="error_msg">' + value + '</span>');
+                });
             }
-    
-            $.ajax({
-                type: 'POST',
-                url: $form.attr('action'),
-                beforeSend: function() {
-                    $('div.form-overlay').fadeIn();
-                },
-                complete: function() {
-                    $('div.form-overlay').fadeOut();
-                },
-                data: $form.serialize()
-            }).always(handleResponse);
-        });
-    
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: $form.attr('action'),
+            beforeSend: function() {
+                $('div.form-overlay').fadeIn();
+            },
+            complete: function() {
+                $('div.form-overlay').fadeOut();
+            },
+            data: $form.serialize()
+        }).always(handleResponse);
+        
+        return false;
+        
     });
-}(window.jQuery));
+    
+});
