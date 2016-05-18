95d7d4da83726f3c66819e589a654c873ae278dc
Matas Dailyda <matas@dailyda.com>
2016-05-18 16:04:17 +0300

    fix urls

--- cmsplugin_contact/urls.py
+++ cmsplugin_contact/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.conf.urls.i18n import i18n_patterns
 from .views import ContactFormAjaxView
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns('',
     url(r'^multi_form/$', ContactFormAjaxView.as_view(), name='multi_form'),
-]
+)
