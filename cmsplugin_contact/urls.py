d3ba6fe06d8b1750e2e06135245c96169722a199
Matas Dailyda <matas@dailyda.com>
2016-07-04 16:23:49 +0300

    revert url.py

--- cmsplugin_contact/urls.py
+++ cmsplugin_contact/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from django.conf.urls.i18n import i18n_patterns
 from .views import ContactFormAjaxView
 
 
-urlpatterns = i18n_patterns('',
+urlpatterns = [
     url(r'^multi_form/$', ContactFormAjaxView.as_view(), name='multi_form'),
-)
+]
